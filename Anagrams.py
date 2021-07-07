ef161dfac49d204a15a209122f34c2c527d6b500
Simon Garcia <gasimon12@gmail.com>
2021-07-06 21:25:49 -0400

    Funcion y Pruebas verificacion anagrams

--- Anagrams.py
+++ Anagrams.py
@@ -1,2 +1,12 @@
 def Sanitize(word):
   return str(sorted(word))
+
+dic = {}
+
+def Check(word):
+  key = Sanitize(word)
+  match = key in dic
+  if not match:
+    dic.setdefault(key, [])
+  dic[key].append(word)
+  return match
--- Anagrams_test.py
+++ Anagrams_test.py
@@ -10,3 +10,15 @@ def test_GetSanitizedTest2():
   word = "coro"
   result = "['c', 'o', 'o', 'r']"
   assert Anagram.Sanitize(word) == result
+
+def test_AreAnagrams_ReturnTrue():
+  word1 = "romo"
+  word2 = "moro"
+  Anagram.Check(word1)
+  assert Anagram.Check(word2) == True
+
+def test_AreNotAnagrams_ReturnFalse():
+  word1 = "pedro"
+  word2 = "papa"
+  Anagram.Check(word1)
+  assert Anagram.Check(word2) == False
