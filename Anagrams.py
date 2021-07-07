346433a8e0cbc0d7c704d1bc9893fb3349e27ca1
Eduardo Melo <49174284+set123ed@users.noreply.github.com>
2021-07-06 21:32:52 -0400

    count anagrams

--- Anagrams.py
+++ Anagrams.py
@@ -10,3 +10,11 @@ def Check(word):
     dic.setdefault(key, [])
   dic[key].append(word)
   return match
+
+
+def count():
+    count = 0
+    for key, value in dic.items():
+        if len(value) > 1:
+            count += 1
+    return count
